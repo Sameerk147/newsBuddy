f291e331e4bab2a68989de989ef5dc611a2a4353
Sameer <sk13042001@gmail.com>
2023-06-11 13:40:07 +0530

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+
+        <footer>
+           copyright <i className="fa-regular fa-copyright"></i> 2023 devloped by sameer khan 8224068598
+        </footer>
+    </div>
+  )
+}
+
+export default Footer;
